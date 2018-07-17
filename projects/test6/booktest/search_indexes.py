d58d2838d80d52bd0568d1e44d80e0e38755d1cb
Yh-desktop <877617594@qq.com>
2018-07-17 16:12:18 +0800

    7/17, test6

---
+++ projects/test6/booktest/search_indexes.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from haystack import indexes
+from models import Test1
+
+
+class Text1Index(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Test1
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
